a1562966fc854e758b314ead57f796247188f1e3
Xeranor <jonas.schulz.12@gmail.com>
2015-09-12 18:31:55 +0200

    Added receive test

--- tests/USART/src/main.c
+++ tests/USART/src/main.c
@@ -6,8 +6,13 @@ int main(void)
 	while(1){
 		usart_transmit_byte('a');
 		usart_transmit_byte('\n');
-		_delay_ms(500);
 		usart_transmit_str("Das ist ein test\n");
+		char test[10];
+		while(1){
+			usart_read_str(test,10);
+			usart_transmit_str(test);
+			usart_transmit_byte('\n');
+		}
 	}
 	return 1;
 }
